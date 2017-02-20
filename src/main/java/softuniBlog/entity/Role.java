43302d6be9467565766f2f030554677264a01f3d
MinhTran <ducminh1712@gmail.com>
2017-02-20 18:24:20 +0700

    Initial commit

---
+++ src/main/java/softuniBlog/entity/Role.java
@@ -0,0 +1,54 @@
+package softuniBlog.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ManyToAny;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	private Integer id;
+	private String name;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	@Column(name = "name", nullable = false)
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	private Set<User> users;
+	
+	public Role() {
+		this.users = new HashSet<>();
+	}
+	
+	@ManyToMany(mappedBy = "roles")
+	public Set<User> getUsers() {
+		return users;
+	}
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+	
+	
+}
